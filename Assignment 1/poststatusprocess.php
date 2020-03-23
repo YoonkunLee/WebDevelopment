51cb40ef45f6d2a29c9550db8a4ceb986e19bf4b
YoonkunLee <ydx9474@aut.ac.nz>
2020-03-23 17:01:08 +1300

    Connect to mysql database

--- Assignment 1/poststatusprocess.php
+++ Assignment 1/poststatusprocess.php
@@ -1,14 +1,29 @@
 <html>
-<head>
-    <title>post process</title>
-</head>
-<body>
-    <?php
-        $code = $_POST["statusCode"];
-        $status = $_POST["status"];
+    <head>
+        <title>search status process</title>
+    </head>
+    <body>
+        <?php
+            require_once('../../conf/sqlinfo.inc.php');
+            $connection = @mysqli_connect(
+                $sql_host, 
+                $sql_user, 
+                $sql_pass, 
+                $sql_db);
+            if(!$connection){
+                die("Connection failed: " . mysqli_connect_error());
+            }
+            echo "Your connection is sucess";
 
-        echo $code;
-        echo $status;
-    ?>
-</body>
+            $sqlString = "CREATE TABLE IF NOT EXISTS post (
+                    status_code VARCHAR(5) NOT NULL,
+                    status_content VARCHAR(255) NOT NULL,
+                    share VARCHAR(20),
+                    input_date DATE NOT NULL,
+                    permission VARCHAR(200))";
+            $queryResult = @mysqli_query($connection, $sqlString) or
+                die("Unable to execute the query." . mysqli_error($connection));
+            echo "Successfully executed the query.";
+        ?>
+    </body>
 </html>
--- Assignment 1/searchstatusprocess.php
+++ Assignment 1/searchstatusprocess.php
@@ -4,6 +4,16 @@
     </head>
     <body>
         <?php
+            require_once('../../conf/sqlinfo.inc.php');
+            $connection = @mysqli_connect(
+                $sql_host, 
+                $sql_user, 
+                $sql_pass, 
+                $sql_db);
+            if(!$connection){
+                die("Connection failed: " . mysqli_connect_error());
+            }
+            echo "Your connection is sucess";
         ?>
     </body>
 </html>
